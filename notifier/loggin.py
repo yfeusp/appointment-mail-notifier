aad3c51e5fd981f2c64ecb69638f6db13f5c432f
Yusdanis Feus Pérez <feus2006@gmail.com>
2022-08-05 15:58:27 -0500

    Implemented the logging module

---
+++ notifier/loggin.py
@@ -0,0 +1,14 @@
+import logging
+from config import log_config
+
+
+def get_logger():
+    logger = logging.getLogger(log_config["log_name"])
+    log_level = log_config["log_level"]
+    if not logger.handlers:
+        logger.setLevel(log_level)
+        ch = logging.StreamHandler()
+        ch.setLevel(log_level)
+        ch.setFormatter(logging.Formatter(log_config["log_format"]))
+        logger.addHandler(ch)
+    return logger
